43ab011d4227b3c13ee89131481c81b43f30ba3d
sebastam97 <sebastianbox97@hotmail.com>
2025-05-30 18:37:45 -0500

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
-import inject from '@rollup/plugin-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    inject({
-      global: ['globalThis', 'global'],
-      Buffer: ['buffer', 'Buffer']
-    })
-  ],
-  base: '/portfolio/',
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis'
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true
-        }),
-        NodeModulesPolyfillPlugin()
-      ]
-    }
-  },
-  resolve: {
-    alias: {
-      crypto: 'crypto-browserify',
-      stream: 'stream-browserify',
-      buffer: 'buffer'
-    }
-  },
-  build: {
-    rollupOptions: {
-      plugins: [
-        inject({
-          global: ['globalThis', 'global'],
-          Buffer: ['buffer', 'Buffer']
-        })
-      ]
-    }
-  }
+  plugins: [react()],
+  base: '/portfolio/'
 });
