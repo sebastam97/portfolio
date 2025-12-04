7b344a74296a96aae629ff49534948876d84f783
sebastam97 <sebastianbox97@hotmail.com>
2025-12-04 16:15:35 -0500

    feat: agregar proyecto 'Generated Image Profile' a la lista de proyectos

--- src/app/portFolio/components/ProjectsComponent/hooks/useGetProjects.tsx
+++ src/app/portFolio/components/ProjectsComponent/hooks/useGetProjects.tsx
@@ -46,6 +46,16 @@ function useGetProjects() {
       liveUrl: 'https://www.smiofi.com/app/invitado',
       repoUrl: '',
       technologies: ['React', 'TypeScript', 'Firebase', 'Material UI', 'Css']
+    },
+    {
+      id: 4,
+      title: 'Generated Image Profile',
+      description:
+        'Una aplicación web que utiliza IA (Gemini 2.5 Flash) para generar fotos de perfil profesionales a partir de selfies, ideal para LinkedIn.',
+      imageUrl: `${import.meta.env.BASE_URL}generate-image.jpg`,
+      liveUrl: 'https://sebastam97.github.io/generated-ImageProfile/',
+      repoUrl: 'https://github.com/sebastam97/generated-ImageProfile',
+      technologies: ['React', 'TypeScript', 'Gemini AI', 'Vite']
     }
   ];
 
